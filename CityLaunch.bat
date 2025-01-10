6c53206e46696ca7fbacf786c86ebc8de57f4fbc
SophiaDetavex <168594979+SophiaDetavex@users.noreply.github.com>
2025-01-10 13:05:06 -0700

    Add files via upload

---
+++ CityLaunch.bat
@@ -0,0 +1,9 @@
+@echo off
+cls
+REM CityLaunch v0.5 by Sophia Shurts
+REM Now launching!
+call "C:\GOG Games\SimCity 4 Deluxe Edition\Apps\SimCity 4.exe" -d:software
+REM Did you enjoy your playtime user :)
+REM pause
+cls
+exit
